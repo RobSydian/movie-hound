96e4ab0908d30cf06d3bef773fbfcf6a55f43477
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2021-09-24 17:15:43 +0200

    Fix arrow index and hover

--- src/components/styles/StyledCarouselControls.js
+++ src/components/styles/StyledCarouselControls.js
@@ -6,12 +6,17 @@ const StyledCarouselControls = styled.div`
   justify-content: space-between;
   position: relative;
   top: 140px;
-  z-index: 1;
 
   & > button {
     background-color: rgba(0, 0, 0, 0.5);
     border: none;
     margin: 5px -1%;
+    z-index: 1;
+    cursor: pointer;
+  }
+
+  & > button:hover {
+    background-color: rgba(0, 0, 0, 0.8);
   }
 `;
 
