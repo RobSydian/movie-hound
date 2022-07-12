20d0fba07215e9dc99f45eef67bd7b552afa7f64
rob <rertus@hotmail.com>
2022-07-12 12:41:14 +0200

    Add spinner to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 import Carousel from './Carousel';
-import { discoverMoviesCached, getGenres } from '../api';
+import RiseLoader from "react-spinners/RiseLoader";
+import { discoverMoviesCached, discoverMovies, getGenres } from '../api';
 import StyledHome from './styles/StyledHome';
 
 export default () => {
-  const [discoverMovies, setDiscoverMovies] = useState([]);
+  const [discoverAllMovies, setDiscoverAllMovies] = useState([]);
   // const [genres, setGenres] = useState([]);
   const [popularMovies, setPopularMovies] = useState([]);
 
@@ -13,15 +14,15 @@ export default () => {
   const [error, setError] = useState(false);
 
   useEffect(async () => {
-    // discoverMovies
-    //   .then((res) => res.json())
-    //   .then((json) => setDiscoverMovies(json.results))
-    //   .catch((e) => console.log(e));
+    discoverMovies()
+      .then((res) => res.json())
+      .then((json) => setDiscoverAllMovies(json.results))
+      .catch((e) => console.log(e));
 
     const responseGenres = await getGenres();
     // setGenres(await responseGenres.json());
 
-    setDiscoverMovies(discoverMoviesCached.results);
+    // setDiscoverAllMovies(discoverMoviesCached.results);
     setPopularMovies(
       discoverMoviesCached.results.filter((movie) => movie.vote_average >= 8)
     );
@@ -38,9 +39,12 @@ export default () => {
   return (
     <StyledHome>
       <h1>Discover</h1>
-      <Carousel items={discoverMovies} />
+      <RiseLoader color="#522B47" cssOverride={{margin:"100px 40%"}} loading={loading} size={50} />
+      {!loading && <Carousel items={discoverAllMovies} />}
       <h1>Popular Movies</h1>
-      <Carousel items={popularMovies} />
+      <RiseLoader color="#522B47" cssOverride={{margin:"100px 40%"}} loading={loading} size={50} />
+      {!loading && <Carousel items={popularMovies} />}
+
       {/* 
       My List
       
