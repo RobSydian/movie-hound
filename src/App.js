aa6469e79ae24b165513488120db652752520bdf
rob <rertus@hotmail.com>
2022-08-06 10:43:14 +0200

    Handle user loggedIn in app

--- src/App.js
+++ src/App.js
@@ -3,26 +3,30 @@ import React, { useEffect } from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-import StyledApp from './StyledApp';
+import StyledApp from './components/styles/StyledApp';
 import { Toaster } from 'react-hot-toast';
 import { useDispatch, useSelector } from 'react-redux';
 import { doc, setDoc } from 'firebase/firestore';
 import { auth, db, getUsersFavList } from './firebase';
 import { addToListActions } from './store/fav-movie-list-handler-slice';
+import { useAuth } from './contexts/AuthContext';
 
 export default () => {
   const userList = useSelector((state) => state.handleList.moviesList);
   const dispatch = useDispatch();
+  const { currentUser } = useAuth();
 
   useEffect(() => {
-    getUsersFavList().then((res) => {
-      res.map((mov) => dispatch(addToListActions.addMovie(mov)));
-    });
-
-    if (userList.length !== 0) {
-      setDoc(doc(db, `users`, `${auth.currentUser.uid}`), {
-        userList,
+    if (currentUser) {
+      getUsersFavList().then((res) => {
+        res.map((mov) => dispatch(addToListActions.addMovie(mov)));
       });
+
+      if (userList.length !== 0) {
+        setDoc(doc(db, `users`, `${auth.currentUser.uid}`), {
+          userList,
+        });
+      }
     }
   }, [userList]);
 
